6dd6e7f5e17af1efd6480f661399785f453ba39e
Chalant <Strikerklm96@gmail.com>
2018-11-08 21:56:03 -0600

    Initial Commit;

---
+++ examples/play_tvz.py
@@ -0,0 +1,14 @@
+import sc2
+from sc2 import Race
+from sc2.player import Human, Bot
+
+from cannon_rush import CannonRushBot
+
+def main():
+    sc2.run_game(sc2.maps.get("Abyssal Reef LE"), [
+        Human(Race.Terran),
+        Bot(Race.Zerg, CannonRushBot())
+    ], realtime=True)
+
+if __name__ == '__main__':
+    main()
