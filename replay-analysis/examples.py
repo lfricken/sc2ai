c4f4c58a3fa4c8fccfb75d3392ac3d4711fbff2a
Chalant <Strikerklm96@gmail.com>
2018-11-08 23:12:29 -0600

    Replay Analyzer Army Value

--- replay-analysis/examples.py
+++ replay-analysis/examples.py
@@ -9,7 +9,25 @@ def test_hots_hatchfun():
 	return sc2reader.load_replay(replay_directory() + "Example.SC2Replay", load_level=4)
 
 
-
 replay = test_hots_hatchfun()
 
-replay.work()
+unit_value = 0
+current_increment = 0
+game_increments = replay.real_length.seconds * 2 * 11
+
+for player in replay.players:
+
+	while current_increment < game_increments:
+
+		for unit in player.units:
+			if unit.minerals > 0:
+				if unit.died_at is not None and unit.finished_at is not None:
+					if unit.finished_at <= current_increment < unit.died_at:
+						unit_value += unit.minerals + unit.vespene
+		print(unit_value)
+		unit_value = 0
+		current_increment += 100
+
+	break
+
+
