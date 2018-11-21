f8c8049ef027c8ab19faef4105a8f8f628c67932
Chalant <Strikerklm96@gmail.com>
2018-11-20 20:43:21 -0600

    Start v3

---
+++ custom/sc2bot_v3/DecisionMaker.py
@@ -0,0 +1,36 @@
+from utils.Investments import Investments
+
+
+class DecisionMaker:
+	"""
+	Decides how to invest money.
+	"""
+
+	def get_target_values(self, heuristic, current_investments: Investments):
+		"""
+		What should our current investments look like? Adds some value onto our current set of investments.
+		:param heuristic: Tells us whether an investment strategy is good or not.
+		:param current_investments: Current investments. If we have 1 Barracks, production would be 150.
+		:return: What our total investments should be.
+		"""
+		target_values = Investments()
+		old_score = -1
+
+		# try different investment strategies
+		for index in range(Investments.num_investment_options()):
+			new_strategy = self.generate_new_strategy(index, current_investments)
+			score = heuristic.get_score(new_strategy)
+
+			# pick the best investment option
+			if score > old_score:
+				old_score = score
+				target_values = new_strategy
+
+		return target_values
+
+	def generate_new_strategy(self, index: int, current_investments: Investments) -> Investments:
+		"""Think of a new way of investing the extra money."""
+		additional_investment = Investments()
+		additional_investment.investments[index] = Investments.investment_amount()
+
+		return current_investments.plus(additional_investment)
---
+++ custom/sc2bot_v3/Tactician.py
@@ -0,0 +1,34 @@
+import sc2
+
+from sc2.constants import UnitTypeId as UnitType
+from sc2.helpers import ControlGroup
+
+
+class Tactician:
+	"""
+	The purpose of the tactician is to make direct military
+	decisions such as attack here, defend there, build turret here.
+	"""
+
+	bot: sc2.BotAI = None
+	attack_groups: [] = None
+	"""Reference to our bot to grab game state data."""
+
+	def __init__(self, bot: sc2.BotAI):
+		self.bot = bot
+		self.attack_groups = set()
+
+	async def update(self):
+
+		if self.bot.units(UnitType.MARINE).idle.amount > 30:
+			cg = ControlGroup(self.bot.units(UnitType.MARINE).idle)
+			self.attack_groups.add(cg)
+
+		for ac in list(self.attack_groups):
+			alive_units = ac.select_units(self.bot.units)
+			if alive_units.exists and alive_units.idle.exists:
+				target = self.bot.known_enemy_structures.random_or(self.bot.enemy_start_locations[0]).position
+				for marine in ac.select_units(self.bot.units):
+					await self.bot.do(marine.attack(target))
+			else:
+				self.attack_groups.remove(ac)
