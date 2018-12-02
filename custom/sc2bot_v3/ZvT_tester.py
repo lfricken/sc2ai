44c8221e8e1571eb2b6dd2edc1916c17f44f4636
Chalant <Strikerklm96@gmail.com>
2018-12-02 14:06:13 -0600

    Multi Plot Working

--- custom/sc2bot_v3/ZvT_tester.py
+++ custom/sc2bot_v3/ZvT_tester.py
@@ -1,5 +1,43 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-plt.plot([1, 2, 3, 6])
-plt.ylabel('some numbers')
-plt.show()
+max_x = 4
+
+
+class PlotValues:
+	label: str
+	color: str
+	style: str
+	data: [int]
+
+	def __init__(self, label, color, style, data):
+		self.label = label
+		self.color = color
+		self.style = style
+		self.data = data
+
+
+def run():
+	lines_to_plot: [PlotValues] = list()
+	lines_to_plot.append(PlotValues("Army", "blue", "-", np.random.randn(4)))
+	lines_to_plot.append(PlotValues("Expand", "red", "--", np.random.randn(4)))
+
+	data = dict()
+	data["x"] = range(0, max_x)
+	for _ in lines_to_plot:
+		line: PlotValues = _
+		data[line.label] = line.data
+
+	df = pd.DataFrame(data)
+
+	for _ in lines_to_plot:
+		line: PlotValues = _
+		plt.plot("x", line.label, data=df, marker="", color=line.color, linewidth=2, linestyle=line.style)
+
+	plt.ylabel("Values")
+	plt.legend()
+	plt.show()
+
+
+run()
