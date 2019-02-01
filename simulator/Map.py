7f5b98ef138d80e7c23b32eb9af4d762edd0e22b
Chalant <strikerklm96@gmail.com>
2019-01-31 21:16:07 -0600

    Make Map A Small Grid

--- simulator/Map.py
+++ simulator/Map.py
@@ -1,76 +1,27 @@
 from Unit import *
 
 
-class Cell:
-	"""Holds units."""
-
-	def add_unit(self, unit: Unit):
-		self._space_taken += unit.size
-		self._units[unit.team].append(unit)
-
-	def remove_unit(self, unit: Unit):
-		self._space_taken -= unit.size
-		self._units[unit.team].remove(unit)
-
-	def contains_team(self, team: Team) -> bool:
-		"""Returns true if this cell contains units from that team."""
-		return len(self._units[team]) > 0
-
-	@property
-	def get_units(self) -> {Team: [Unit]}:
-		"""How much of this cell is taken up already."""
-		return self._units
-
-	@staticmethod
-	def get_cell_size() -> int:
-		"""How big is a cell in this map vs normal sc2?"""
-		return 2
-
-	@staticmethod
-	def _get_cell_space() -> int:
-		"""How much stuff can a cell hold?"""
-		return Cell.get_cell_size() * Cell.get_cell_size()
-
-	def __init__(self):
-		self._space_taken = 0.0
-		self._units: {Team: [Unit]} = {Team.Blue: [], Team.Red: []}
-		self.raised = False
-		self.cliff = False
-		self.void = False
-
-	raised: bool
-	"""True if we can move over cliffs."""
-	"""Ramps are merely raised terrain that joins with lower terrain without impassable."""
-
-	cliff: bool
-	"""This is a cliff."""
-
-	void: bool
-	"""This is void (only air units can go here)."""
-
-	def will_fit(self, unit: Unit) -> bool:
-		"""Returns true if this unit will fit in this cell."""
-		return (self._space_taken + unit.size) <= Cell._get_cell_space()
-
-	@property
-	def space_taken(self) -> float:
-		"""How much of this cell is taken up already."""
-		return self._space_taken
-
-	@space_taken.setter
-	def space_taken(self, value: float):
-		"""How much of this cell is taken up already."""
-		self._space_taken = value
+class TerrainFlags(Enum):
+	cliff = 1
+	void = 2
+	raised = 4
+	filled = 8
 
 
 class Map:
 	"""Map as a grid of cells."""
 
 	def __init__(self, size: (int, int)):
-
 		# fill grid with new cells
-		self.grid: [[Cell]] = []
+		self.grid: [bytearray] = []
 		for i in range(size[0]):
-			self.grid.append([])
-			for j in range(size[1]):
-				self.grid[i].append(Cell())
+			self.grid.append(bytearray(size[1]))
+
+	def set_type(self, position: (int, int), terrain_type: TerrainFlags):
+		self.grid[position[0]][position[1]] |= terrain_type
+
+	def unfill(self, position: (int, int)):
+		self.grid[position[0]][position[1]] = self.grid[position[0]][position[1]] & (TerrainFlags.cliff | TerrainFlags.void | TerrainFlags.raised)
+
+	def is_type(self, position: (int, int), terrain_type: TerrainFlags) -> bool:
+		return bool(self.grid[position[0]][position[1]] | terrain_type)
--- simulator/Tests.py
+++ simulator/Tests.py
@@ -4,6 +4,34 @@ import unittest
 
 
 class SimulatorTest(unittest.TestCase):
+	def test_MapWorks(self):
+		sim = Simulator()
+
+		# terrain lets us set its values
+		pos = (1, 1)
+		sim.map.set_type(pos, TerrainFlags.raised)
+		self.assertTrue(sim.map.is_type(pos, TerrainFlags.raised))
+
+		# most terrain is empty
+		pos = (0, 0)
+		self.assertFalse(sim.map.is_type(pos, TerrainFlags.raised))
+		self.assertFalse(sim.map.is_type(pos, TerrainFlags.void))
+		self.assertFalse(sim.map.is_type(pos, TerrainFlags.cliff))
+		self.assertFalse(sim.map.is_type(pos, TerrainFlags.filled))
+
+		# filling
+		pos = (0, 0)
+		self.assertFalse(sim.map.set_type(pos, TerrainFlags.filled))
+		sim.map.set_type(pos, TerrainFlags.filled)
+		self.assertTrue(sim.map.set_type(pos, TerrainFlags.filled))
+
+		# unfilling
+		pos = (0, 0)
+		sim.map.set_type(pos, TerrainFlags.filled)
+		self.assertTrue(sim.map.set_type(pos, TerrainFlags.filled))
+		sim.map.unfill(pos)
+		self.assertFalse(sim.map.set_type(pos, TerrainFlags.filled))
+
 	def test_AddUnitAddsBoth(self):
 		sim = Simulator()
 		position = (0, 0)
@@ -11,10 +39,8 @@ class SimulatorTest(unittest.TestCase):
 		team: Team = Team.Red
 
 		sim.try_add_unit("Marine", position, team)
-		cell_units: [Unit] = sim.get_cell(position).get_units
 
 		self.assertEqual(len(sim.units[team]), expected_number)
-		self.assertEqual(len(cell_units[team]), expected_number)
 
 	def test_InRange(self):
 		sim = Simulator()
@@ -70,6 +96,21 @@ class SimulatorTest(unittest.TestCase):
 
 		self.assertTrue(not sim.units[Team.Red][0].alive)
 		self.assertTrue(not sim.units[Team.Red][1].alive)
+
+	def test_NoOverfill(self):
+		sim = Simulator()
+
+		sim.try_add_unit("Marine", (0, 0), Team.Blue)
+		sim.try_add_unit("Marine", (0, 0), Team.Blue)
+		sim.try_add_unit("Marine", (0, 0), Team.Blue)
+		sim.try_add_unit("Marine", (0, 0), Team.Blue)
+		five_added = sim.try_add_unit("Marine", (0, 0), Team.Blue)
+		six_added = sim.try_add_unit("Marine", (0, 0), Team.Blue)
+		six_enemy_added = sim.try_add_unit("Marine", (0, 0), Team.Red)
+
+		self.assertTrue(five_added)
+		self.assertTrue(not six_added)
+		self.assertTrue(not six_enemy_added)
 
 
 if __name__ == '__main__':
